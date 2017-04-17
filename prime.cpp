f096d9c91629e43f8d39c268f351364309e54272
alexseppar <seppar73@gmail.com>
2017-04-17 23:01:28 +0300

    updated

--- prime.cpp
+++ prime.cpp
@@ -2,32 +2,56 @@
 using std::cout;
 using std::endl;
 
-template <bool state, bool Ta, bool Tb>
+template <bool state, typename Ta, typename Tb>
 struct ITE;
 
-template <bool Ta, bool Tb>
+template <typename Ta, typename Tb>
 struct ITE<true, Ta, Tb> {
-    static const bool value = Ta;
+    typedef Ta res;
 };
 
-template <bool Ta, bool Tb>
+template <typename Ta, typename Tb>
 struct ITE<false, Ta, Tb> {
-    static const bool value = Tb;
+    typedef Tb res;
+};
+
+template <int N, int LO = 1, int HI = N>
+struct Sqrt {
+    enum { mid = (LO + HI + 1) / 2 }; 
+    typedef typename ITE<(N < mid * mid), Sqrt<N, LO, mid - 1>, 
+                                          Sqrt<N, mid, HI>     >::res RES;
+    enum { res = RES::res };
+};
+
+template <int N, int S>
+struct Sqrt <N, S, S> {
+    enum { res = S };
+};
+
+template <typename T, T val>
+struct constant {
+    typedef T type;
+    static const T value = val;
+};
+
+using true_type = constant<bool, true>;
+
+using false_type = constant<bool, false>;
+
+template <size_t N, size_t count, size_t check>
+struct is_prime_helper {
+    typedef typename ITE <N % count == 0, false_type, is_prime_helper<N, count + 1, check>>::res RES;
+    static const bool value = RES::value;
 };
 
 template <size_t N, size_t count>
-struct is_prime_helper {
-    static const bool value = ITE <N % count == 0, false, is_prime_helper<N, count + 1>::value>::value;
-};
-
-template <size_t N>
-struct is_prime_helper<N, N> {
+struct is_prime_helper<N, count, count> {
     static const bool value = true;
 };
 
 template <size_t N>
 struct is_prime {
-    static const bool value = is_prime_helper<N, 2>::value;
+    static const bool value = is_prime_helper<N, 2, Sqrt<N>::res + 1>::value;
 };
 
 template <size_t N, size_t count, size_t product, bool prime>
@@ -40,17 +64,22 @@ struct helper<N, N, product, true> {
 
 template <size_t N, size_t count, size_t product>
 struct helper<N, count, product, true> {
-    enum { val = helper<N, count + 1, product + 1, is_prime<product + 1>::value>::val };
+    enum { val = helper<N, count + 1, product + 2, is_prime<product + 2>::value>::val };
 };
 
 template <size_t N, size_t count, size_t product>
 struct helper<N, count, product, false> {
-    enum { val = helper<N, count, product + 1, is_prime<product + 1>::value>::val };
+    enum { val = helper<N, count, product + 2, is_prime<product + 2>::value>::val };
 };
 
 template <size_t N>
 struct Nth_prime {
-    enum { value = helper <N, 1, 2, true>::val };
+    enum { value = helper <N, 2, 3, true>::val };
+};
+
+template <>
+struct Nth_prime<1> {
+    enum { value = 2 };
 };
 
 #define check(num) \
@@ -58,16 +87,6 @@ struct Nth_prime {
 
 int main ()
 {
-    check(1)
-    check(2)
-    check(3)
-    check(4)
-    check(5)
-    check(6)
-    check(7)
-    check(8)
-    check(9)
-    check(10)
-    check(11)
+    check(100)
     return 0;
 }
